8f15b29d59490509c9f20258214796cfba468208
jihwooon <jihwooon@gmail.com>
2022-11-13 15:55:45 +0900

    불리언 타입 알아보기

--- ch01/app.ts
+++ ch01/app.ts
@@ -1,15 +1,17 @@
-function add(n1: number, n2: number) {
-    if(typeof n1 === 'number' && typeof n2 === 'number') {
-        console.log('n1 type: ', typeof n1);
-        return n1 + n2;
+function add(n1: number, n2: number, showResult: boolean, phrase: string) {
+    
+    const result = n1 + n2;
+    if(showResult) {
+        console.log(phrase + result);
     }
-    return new Error('Incorrect input!');
+    return result;
 }
 
 const number1 = 5;
 const number2 = 2.8;
+const printResult = true;
+const resultPhrase = 'Result is: '
 
 // 타입 에러
-const result = add(number1, number2);
+const result = add(number1, number2, printResult, resultPhrase);
 console.log('result: ', result);
-
