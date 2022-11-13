696e6503e7aa307fee0a707b3e28c6a7e97dc420
jihwooon <jihwooon@gmail.com>
2022-11-14 00:21:03 +0900

    튜플 작업하라
    튜플은 길이와 타입이 고정 된 배열이다.
    []안에 서로 다른 타입을 정할 수 있다.

--- ch01/app.ts
+++ ch01/app.ts
@@ -1,11 +1,21 @@
-const user1 = {
+const user1: {
+    name: string,
+    age: number,
+    hobbies: string[],
+    role: [number, string]
+
+} = {
     name: 'Maximilian',
     age: 30,
-    hobbies: ['Sports', 'Cooking']
+    hobbies: ['Sports', 'Cooking'],
+    role: [2, 'author']
 };
 
+user1.role.push('admin');
+user1.role[1] = 'user';
+
 let favoriteActivities: string[];
-favoriteActivities = ['Sport', 'Cok'];
+favoriteActivities = ['Sport', 'Cok', 'dd', 'dd'];
 
 console.log(user1.name);
 console.log(user1.age);
@@ -15,4 +25,3 @@ console.log(favoriteActivities);
 for(const hobby of user1.hobbies) {
     console.log(hobby.toUpperCase());
 }
-
