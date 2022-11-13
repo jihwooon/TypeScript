e1157084286fb35e1df3522f5ffd999b3fa2273b
jihwooon <jihwooon@gmail.com>
2022-11-13 15:17:19 +0900

    TypeScript의 typeof 기능
    자바스크립트에 숫자형 타입이 있다는 것을 typeof 통해서 확인 할 수 있습니다.
    typeof 기능 덕분에 바닐라 자바스크립트의 함수도 향상 할 수 있습니다.
    console.log('n1 type: ', typeof n1);  입력하고 코드를 실행 했을 때 n1 의 타입이 출력이 됩니다.
    
    조건문에 typeof가 number인지 아닌지도 확인 할 수 있습니다. number 타입이 일치 한다면 조건문을 통과 하지만 일치 하지 않을 경우 에러를 발생합니다.

--- ch01/app.js
+++ ch01/app.js
@@ -13,6 +13,18 @@ var user1 = {
     hobbies: ['Sports', 'Cooking'],
     role: Role.ADMIN
 };
+function add(n1, n2) {
+    if (typeof n1 === 'number' && typeof n2 === 'number') {
+        console.log('n1 type: ', typeof n1);
+        return n1 + n2;
+    }
+    return new Error('Incorrect input!');
+}
+var number1 = 5;
+var number2 = 2.8;
+// 타입 에러
+var result = add(number1, number2);
+console.log('result: ', result);
 // user1.role.push('admin');
 // user1.role[1] = 'user';
 var favoriteActivities;
--- ch01/app.ts
+++ ch01/app.ts
@@ -18,6 +18,21 @@ const user1: {
     role: Role.ADMIN
 };
 
+function add(n1: number, n2: number) {
+    if(typeof n1 === 'number' && typeof n2 === 'number') {
+        console.log('n1 type: ', typeof n1);
+        return n1 + n2;
+    }
+    return new Error('Incorrect input!');
+}
+
+const number1 = 5;
+const number2 = 2.8;
+
+// 타입 에러
+const result = add(number1, number2);
+console.log('result: ', result);
+
 // user1.role.push('admin');
 // user1.role[1] = 'user';
 
