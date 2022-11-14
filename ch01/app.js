0e7de1630d6bab593018559ed401913c1285b408
jihwooon <jihwooon@gmail.com>
2022-11-14 23:56:07 +0900

    열거형으로 작업하기

--- ch01/app.js
+++ ch01/app.js
@@ -1,13 +1,22 @@
+"use strict";
+exports.__esModule = true;
+var combination_1 = require("./combination");
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 2] = "AUTHOR";
+})(Role || (Role = {}));
 var user1 = {
     name: 'Maximilian',
     age: 30,
     hobbies: ['Sports', 'Cooking'],
-    role: [2, 'author']
+    role: Role.ADMIN
 };
-user1.role.push('admin');
-user1.role[1] = 10;
+// user1.role.push('admin');
+// user1.role[1] = 'user';
 var favoriteActivities;
-favoriteActivities = ['Sport', 'Cok'];
+favoriteActivities = ['Sport', 'Cok', 'dd', 'dd'];
 console.log(user1.name);
 console.log(user1.age);
 console.log(favoriteActivities);
@@ -15,3 +24,5 @@ for (var _i = 0, _a = user1.hobbies; _i < _a.length; _i++) {
     var hobby = _a[_i];
     console.log(hobby.toUpperCase());
 }
+var combinedAges = (0, combination_1.combine)(30, 20);
+console.log('조합: ', combinedAges);
