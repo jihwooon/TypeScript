bb62ef48397458771c68d74ee33fca35d122bf16
jihwooon <jihwooon@gmail.com>
2022-11-15 01:53:33 +0900

    TypeScript의 typeof 기능 알아보기

--- ch01/app.js
+++ ch01/app.js
@@ -29,14 +29,14 @@ var combinedAges = (0, combination_1.combine)(30, 20);
 console.log('조합: ', combinedAges);
 =======
 function add(n1, n2) {
-    if (typeof n1 === 'number' && typeof n2 === 'number') {
-        console.log('n1 type: ', typeof n1);
-        return n1 + n2;
-    }
-    return new Error('Incorrect input!');
+    // if(showResult) {
+    //     console.log(n1 + n2);
+    // }
+    return n1 + n2;
 }
 var number1 = 5;
 var number2 = 2.8;
+var printResult = true;
 // 타입 에러
 var result = add(number1, number2);
 <<<<<<< HEAD
--- ch01/app.ts
+++ ch01/app.ts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { combine } from './combination';
 enum Role {
     ADMIN,
@@ -39,14 +40,24 @@ function add(n1: number, n2: number) {
     if(typeof n1 === 'number' && typeof n2 === 'number') {
         console.log('n1 type: ', typeof n1);
         return n1 + n2;
+=======
+function add(n1: number, n2: number, showResult: boolean, phrase: string) {
+    
+    const result = n1 + n2;
+    if(showResult) {
+        console.log(phrase + result);
+>>>>>>> 8f15b29 (불리언 타입 알아보기)
     }
-    return new Error('Incorrect input!');
+    return result;
 }
 
 const number1 = 5;
 const number2 = 2.8;
+const printResult = true;
+const resultPhrase = 'Result is: '
 
 // 타입 에러
+<<<<<<< HEAD
 const result = add(number1, number2);
 <<<<<<< HEAD
 console.log(result);
@@ -54,3 +65,7 @@ console.log(result);
 console.log('result: ', result);
 
 >>>>>>> bb38d27 (TypeScript의 typeof 기능 알아보기)
+=======
+const result = add(number1, number2, printResult, resultPhrase);
+console.log('result: ', result);
+>>>>>>> 8f15b29 (불리언 타입 알아보기)
