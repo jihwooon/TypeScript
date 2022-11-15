d78acc1fffc2aa116a8f1382dadedb330973788e
jihwooon <jihwooon@gmail.com>
2022-11-15 22:16:30 +0900

    화살표 함수

---
+++ ch03/app.ts
@@ -0,0 +1,19 @@
+const userName = 'Max';
+
+let age = 30;
+
+age = 28;
+
+console.log(age);
+
+if(age > 20) {
+  let isOld = true;
+}
+
+const add = (a: number, b: number) => a + b;
+
+const printOutput: (a: number | string) => void = output => console.log(output);
+
+console.log(add(2, 5));
+
+
