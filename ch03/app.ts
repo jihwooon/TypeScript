447df1d2bbac2d78844be127b59c6ee78c408aff
jihwooon <jihwooon@gmail.com>
2022-11-28 00:45:46 +0900

    화살표 함수를 표현하라

--- ch03/app.ts
+++ ch03/app.ts
@@ -9,21 +9,16 @@ console.log(age);
 if(age > 20) {
   let isOld = true;
 }
-
 const add = (a: number, b: number) => a + b;
 
-const printOutput: (a: number | string) => void = output => console.log(output);
+console.log(add(2,5));
 
-console.log(add(2, 5));
+const printOutput: (output: string | number) => void = output => console.log(output);
 
-const hobbies = ['Sports', 'Cooking'];
-const activeHobbies = ['Hiking'];
+const button = document.querySelector('button');
 
-activeHobbies.push(...hobbies);
+if (button) {
+  button.addEventListener('click', event => console.log(event));
+}
 
-const person = {
-  name: 'Max',
-  age: 30  
-};
-
-const copiedPerson = { ...person };
+printOutput(add(2,5))
