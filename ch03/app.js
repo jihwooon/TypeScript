f605e4f47e8eb265f924975c610316369cecefb5
jihwooon <jihwooon@gmail.com>
2022-12-06 22:03:41 +0900

    스프레드 연산자를 공부하라
    ... 표현은 배열을 뜻합니다.
    배열로 되어 있는 변수를 (... 변수)로 표현 할 수 있습니다.

--- ch03/app.js
+++ ch03/app.js
@@ -5,5 +5,19 @@ console.log(age);
 if (age > 20) {
     var isOld = true;
 }
-var add = function (a, b) { return a + b; };
-console.log(add(2, 5));
+var add = function (a, b) {
+    if (b === void 0) { b = 1; }
+    return a + b;
+};
+console.log(add(5));
+var printOutput = function (output) { return console.log(output); };
+var button = document.querySelector('button');
+if (button) {
+    button.addEventListener('click', function (event) { return console.log(event); });
+}
+printOutput(add(5));
+var hobbies = ['Sports', 'Cooking'];
+console.log(hobbies[0]);
+var activeHobbies = ['Hiking'];
+activeHobbies.push.apply(activeHobbies, hobbies);
+console.log(activeHobbies);
