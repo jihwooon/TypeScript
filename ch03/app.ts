e46cf557359b3bf182e7b0680aeae9955d2273ad
jihwooon <jihwooon@gmail.com>
2022-12-06 22:22:55 +0900

    배열 및 객체 비구조화 할당

--- ch03/app.ts
+++ ch03/app.ts
@@ -1,14 +1,14 @@
-const userName = 'Max';
-
-let age = 30;
-
-age = 28;
-
-console.log(age);
-
-if(age > 20) {
-  let isOld = true;
-}
+// const userName = 'Max';
+//
+// let age = 30;
+//
+// age = 28;
+//
+// console.log(age);
+//
+// if(age > 20) {
+//   let isOld = true;
+// }
 const add = (a: number, b: number = 1) => a + b;
 
 console.log(add(5));
@@ -32,7 +32,7 @@ activeHobbies.push(...hobbies);
 console.log(activeHobbies);
 
 const person = {
-  name: 'Max',
+  firstName: 'Max',
   age: 30
 };
 
@@ -46,3 +46,9 @@ const addReduce = (...numbers: number[]) => {
 
 const addedNumbers = addReduce(5,10,2);
 console.log(addedNumbers);
+
+//TODO: 배열 및 비구조화 할당
+const [hobby1, hobby2, ...remainingHobbies] = hobbies;
+
+const { firstName: userName, age } = person;
+console.log(userName, age, person);
