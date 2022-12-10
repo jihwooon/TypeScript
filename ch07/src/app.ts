74d0699a2197411ad70020617b47e505817d737b
jihwooon <jihwooon@gmail.com>
2022-12-11 03:01:29 +0900

    다른 일반 함수

--- ch07/src/app.ts
+++ ch07/src/app.ts
@@ -25,3 +25,20 @@ const mergedObj = merge({ name: 'Max', hobbies: ['Sports'] }, { age: 30 });
 
 console.log(mergedObj.objA);
 console.log(mergedObj.age);
+
+interface Lengthy {
+    length: number;
+}
+
+function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
+    let descriptionText = 'Got no value.';
+    if (element.length === 1)  {
+        descriptionText = 'Got 1 element.';
+    } else if (element.length > 1) {
+        descriptionText = 'Got ' + element.length + ' elements.';
+    }
+
+    return [element, descriptionText];
+}
+
+console.log(countAndDescribe('Hi There!'));
