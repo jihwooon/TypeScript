4b63425ebe37002d015caf3a00a43207d4f67907
jihwooon <jihwooon@gmail.com>
2022-12-11 02:15:37 +0900

    제약 조건 작업하기

--- ch07/src/app.ts
+++ ch07/src/app.ts
@@ -17,7 +17,7 @@ console.log("Hello Typescript");
 //     data.split(' ');
 // });
 
-function merge<T, U>(objA: T, objB: U) {
+function merge<T extends object, U extends object >(objA: T, objB: U) {
     return Object.assign({ objA }, objB);
 }
 
