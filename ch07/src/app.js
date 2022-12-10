d375d82de4c43d01c12fff4e5a9c80175fe009ee
jihwooon <jihwooon@gmail.com>
2022-12-11 01:54:21 +0900

    제네렉 함수 생성하기
    TypeScript 제네릭을 생성하라

--- ch07/src/app.js
+++ ch07/src/app.js
@@ -1,14 +1,19 @@
-"use strict";
 console.log("Hello Typescript");
-const names = [];
-names[0].split(' ');
-names.push('abc');
-console.log(names);
-const promise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        resolve('Time is gone');
-    }, 2000);
-});
-promise.then(data => {
-    data.split(' ');
-});
+// const names: Array<string> = [];
+// names[0].split(' ');
+// names.push('abc');
+// console.log(names);
+// const promise = new Promise<string>((resolve, reject) => {
+//     setTimeout(() => {
+//         resolve('Time is gone');
+//     }, 2000);
+// })
+// promise.then(data => {
+//     data.split(' ');
+// });
+function merge(objA, objB) {
+    return Object.assign({ objA: objA }, objB);
+}
+var mergedObj = merge({ name: 'Max', hobbies: ['Sports'] }, { age: 30 });
+console.log(mergedObj.objA);
+console.log(mergedObj.age);
--- ch07/src/app.ts
+++ ch07/src/app.ts
@@ -1,18 +1,27 @@
 console.log("Hello Typescript");
 
-const names: Array<string> = [];
-names[0].split(' ');
+// const names: Array<string> = [];
+// names[0].split(' ');
 
-names.push('abc');
+// names.push('abc');
 
-console.log(names);
+// console.log(names);
 
-const promise = new Promise<string>((resolve, reject) => {
-    setTimeout(() => {
-        resolve('Time is gone');
-    }, 2000);
-})
+// const promise = new Promise<string>((resolve, reject) => {
+//     setTimeout(() => {
+//         resolve('Time is gone');
+//     }, 2000);
+// })
 
-promise.then(data => {
-    data.split(' ');
-});
+// promise.then(data => {
+//     data.split(' ');
+// });
+
+function merge<T, U>(objA: T, objB: U) {
+    return Object.assign({ objA }, objB);
+}
+
+const mergedObj = merge({ name: 'Max', hobbies: ['Sports'] }, { age: 30 });
+
+console.log(mergedObj.objA);
+console.log(mergedObj.age);
