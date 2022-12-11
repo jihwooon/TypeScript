daebb203472b62b3bbc7ab32effad2eeceae2331
jihwooon <jihwooon@gmail.com>
2022-12-12 02:02:57 +0900

    제네릭 유틸리티 타입을 추가하라

--- ch07/src/app.ts
+++ ch07/src/app.ts
@@ -83,3 +83,25 @@ numberStorage.addItem(1);
 // objectStorage.addItem({name: 'Menu'});
 
 // console.log(objectStorage.getItems());
+
+interface CourseGoal {
+    title: string;
+    description: string;
+    completeUntil: Date;
+}
+
+function createCourseGoal(
+    title: string, 
+    description: string, 
+    date: Date
+): CourseGoal {
+    let courseGoal: Partial<CourseGoal> = {};
+    
+    courseGoal.title = title;
+    courseGoal.description = description;
+    courseGoal.completeUntil = date;
+
+    return courseGoal as CourseGoal;
+}
+
+const names: Readonly<string[]> = ['Max', 'Anna'];
