bc5994f2f1ea7ad37d7d8fcf5207fc6d7fbbe4e8
jihwooon <jihwooon@gmail.com>
2022-12-11 10:27:35 +0900

    package dependencies를 추가하라
    ts-node, tsc-watch 라이브러를 추가했습니다.

--- ch07/src/app.js
+++ ch07/src/app.js
@@ -1,24 +1,25 @@
+"use strict";
 console.log("Hello Typescript");
 // const names: Array<string> = [];
 // names[0].split(' ');
 // names.push('abc');
 // console.log(names);
-// const promise = new Promise<string>((resolve, reject) => {
-//     setTimeout(() => {
-//         resolve('Time is gone');
-//     }, 2000);
-// })
-// promise.then(data => {
-//     data.split(' ');
-// });
+const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve('Time is gone');
+    }, 2000);
+});
+promise.then(data => {
+    data.split(' ');
+});
 function merge(objA, objB) {
-    return Object.assign({ objA: objA }, objB);
+    return Object.assign(objA, objB);
 }
-var mergedObj = merge({ name: 'Max', hobbies: ['Sports'] }, { age: 30 });
-console.log(mergedObj.objA);
+const mergedObj = merge({ name: 'Max', hobbies: ['Sports'] }, { age: 30 });
+console.log(mergedObj.hobbies);
 console.log(mergedObj.age);
 function countAndDescribe(element) {
-    var descriptionText = 'Got no value.';
+    let descriptionText = 'Got no value.';
     if (element.length === 1) {
         descriptionText = 'Got 1 element.';
     }
