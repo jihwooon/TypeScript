5dad0e5f0a7e902bd7fcce7a28a84b7d998aa42b
jihwooon <jihwooon@gmail.com>
2022-12-12 01:43:00 +0900

    제네릭 클래스를 공부하라
    제네릭 타입은 타입 안정성과 결합된 유연성을 제공합니다.

--- ch07/src/app.ts
+++ ch07/src/app.ts
@@ -42,3 +42,44 @@ function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
 }
 
 console.log(countAndDescribe('Hi There!'));
+
+class DataStorage<T extends string | number | boolean> {
+    private data: T[] = [];
+
+    addItem(item: T) {
+        this.data.push(item);
+    }
+
+    removeItem(item: T) {
+        if (this.data.indexOf(item) === -1) return;
+        this.data.splice(this.data.indexOf(item), 1);
+    }
+
+    getItems() {
+        return [...this.data];
+    }
+}
+
+const textStorage = new DataStorage<string>();
+textStorage.addItem('Max');
+textStorage.addItem('Manu');
+textStorage.addItem('Max');
+
+console.log(textStorage.getItems());
+
+const numberStorage = new DataStorage<number>();
+numberStorage.addItem(1);
+numberStorage.addItem(2);
+numberStorage.addItem(3);
+
+console.log(numberStorage.getItems());
+
+const isStorage = new DataStorage<boolean>();
+numberStorage.addItem(1);
+
+
+// const objectStorage = new DataStorage<object>();
+// objectStorage.addItem({name: 'Max'});
+// objectStorage.addItem({name: 'Menu'});
+
+// console.log(objectStorage.getItems());
