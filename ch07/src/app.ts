fb1cefba8e348da4faba2b619a5dd42dcdaea843
jihwooon <jihwooon@gmail.com>
2022-12-11 10:27:35 +0900

    package dependencies를 추가하라
    ts-node, tsc-watch 라이브러를 추가했습니다.

--- ch07/src/app.ts
+++ ch07/src/app.ts
@@ -7,23 +7,23 @@ console.log("Hello Typescript");
 
 // console.log(names);
 
-// const promise = new Promise<string>((resolve, reject) => {
-//     setTimeout(() => {
-//         resolve('Time is gone');
-//     }, 2000);
-// })
+const promise = new Promise<string>((resolve, reject) => {
+    setTimeout(() => {
+        resolve('Time is gone');
+    }, 2000);
+})
 
-// promise.then(data => {
-//     data.split(' ');
-// });
+promise.then(data => {
+    data.split(' ');
+});
 
 function merge<T extends object, U extends object >(objA: T, objB: U) {
-    return Object.assign({ objA }, objB);
+    return Object.assign(objA , objB);
 }
 
 const mergedObj = merge({ name: 'Max', hobbies: ['Sports'] }, { age: 30 });
 
-console.log(mergedObj.objA);
+console.log(mergedObj.hobbies);
 console.log(mergedObj.age);
 
 interface Lengthy {
