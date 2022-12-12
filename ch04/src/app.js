3b2805f80423c87208dd04e75163adb4fdc9f11b
jihwooon <jihwooon@gmail.com>
2022-12-12 13:02:05 +0900

    퍼스트 클래스를 생성하라

--- ch04/src/app.js
+++ ch04/src/app.js
@@ -1,2 +1,9 @@
 "use strict";
 console.log('Hello TypeScript');
+class Department {
+    constructor(name) {
+        this.name = name;
+    }
+}
+const newDepartment = new Department('Accounting');
+console.log(newDepartment.name);
--- ch04/src/app.ts
+++ ch04/src/app.ts
@@ -1,1 +1,13 @@
 console.log('Hello TypeScript');
+
+class Department {
+    name: string;
+
+    constructor(name: string) {
+        this.name = name;
+    }
+}
+
+const newDepartment = new Department('Accounting');
+
+console.log(newDepartment.name);
