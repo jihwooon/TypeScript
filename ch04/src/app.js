0380406580942fa1d2cb764e1c02d4c0b422ed4e
jihwooon <jihwooon@gmail.com>
2022-12-12 13:14:54 +0900

    생성자 함수 및 this 키워드를 공부하라

--- ch04/src/app.js
+++ ch04/src/app.js
@@ -2,8 +2,16 @@
 console.log('Hello TypeScript');
 class Department {
     constructor(name) {
+        this.name = 'Deault';
         this.name = name;
+    }
+    describe() {
+        return this.name;
     }
 }
 const newDepartment = new Department('Accounting');
 console.log(newDepartment.name);
+console.log(newDepartment.describe());
+2;
+const accountingCopy = { name: 'DUMMY', describe: newDepartment.describe() };
+console.log(accountingCopy);
--- ch04/src/app.ts
+++ ch04/src/app.ts
@@ -1,13 +1,22 @@
 console.log('Hello TypeScript');
 
 class Department {
-    name: string;
+    name: string = 'Deault';
 
     constructor(name: string) {
         this.name = name;
+    }
+
+    describe() {
+        return this.name;
     }
 }
 
 const newDepartment = new Department('Accounting');
 
 console.log(newDepartment.name);
+console.log(newDepartment.describe());2
+
+const accountingCopy = { name: 'DUMMY', describe: newDepartment.describe()};
+
+console.log(accountingCopy);
